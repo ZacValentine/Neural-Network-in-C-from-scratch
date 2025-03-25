88ab83fd3f8638e9b96ace2967f52e87a9fbaedb
Zac Valentine <112518832+ZacValentine@users.noreply.github.com>
2025-03-25 17:05:24 -0400

    Update model.c

--- model.c
+++ model.c
@@ -2,12 +2,9 @@
 #include <stdlib.h>
 #define USE_MNIST_LOADER // ONLY IMPORT/CODE THAT IS NOT MINE
 #define MNIST_DOUBLE // ONLY IMPORT/CODE THAT IS NOT MINE
-#include "mnist.h" // ONLY IMPORT/CODE THAT IS NOT MINE
+#include "mnist.h"
 #include "functions.h"
 
-// gcc -o output model.c functions.c
-
-// cant seem to get this one to work in the .h file
 double** getBatch(mnist_data data, int dataNumRows, int dataNumCols, int inputNumRows, int inputNumCols){
     double** input = generateZero2dArray(inputNumRows, inputNumCols);
     for (int i = 0; i < dataNumRows; i++){
@@ -19,7 +16,6 @@ double** getBatch(mnist_data data, int dataNumRows, int dataNumCols, int inputNu
 }
 
 // constants
-// #define filename "saved/loss.csv" // file to save data to
 #define mnistNumRows 28 // number of rows and rows in mnist 2d image
 #define mnistNumCols 28
 #define inputDim (mnistNumRows*mnistNumCols) // flattened 2d pixel array
@@ -47,18 +43,13 @@ double** getBatch(mnist_data data, int dataNumRows, int dataNumCols, int inputNu
 #define probsNumCols outputDim
 
 int main(){
-    // setup data loading - ONLY CODE THAT IS NOT MINE
     mnist_data *data;
     unsigned int cnt;
     int ret;
     mnist_load("data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte", &data, &cnt);
     printf("number of train images: %d\n", cnt);
 
-    // random
     srand(7);
-
-    // make stuct that contains the array, the shape of the array, and functions to modify them dynamically(appending, etc.)
-    // make stuct that contains the model and uses the dynamic array struct for the parameters
 
     // tweakable parameters initialization
     double** weights1 = generateRandom2dArray(weights1NumRows, weights1NumCols);
@@ -86,7 +77,6 @@ int main(){
     int numEpochs = 1;
     for (int epoch = 0; epoch < numEpochs; epoch++){
         for (int step = 0; step < numTrainImages; step++){
-            // examine each function for effeciency, get better print statements + visualization + valdiation + etc., make it clean
             // forward pass
             input = getBatch(data[step], mnistNumRows, mnistNumCols, inputNumRows, inputNumCols);
             target = oneHotEncode(data[step].label, probsNumRows, probsNumCols);
@@ -104,13 +94,12 @@ int main(){
             print2dDoubleArray(probs, probsNumRows, probsNumCols);
     
             // backpropagation(calculate gradients manually)
-            // how much does probs affect the loss through the meanSquaredError function... * dloss = how much it affects the loss
             for (int i = 0; i < probsNumRows; i++){
                 for (int j = 0; j < probsNumCols; j++){
                     dprobs[i][j] = (-2.0/probsNumCols) * (target[i][j] - probs[i][j]);
                 }
             }
-            // how much does logits affect probs through the softmax function... * dprobs = how much it affects the loss
+
             for (int row = 0; row < logitsNumRows; row++){
                 for (int i = 0; i < logitsNumCols; i++){
                     for (int j = 0; j < logitsNumCols; j++){
@@ -122,7 +111,7 @@ int main(){
                     }
                 }
             }
-            // how much does biasOutput affect logits through relu... * dlogits = how much it affects the loss
+
             for (int i = 0; i < biasOutputNumRows; i++){
                 for (int j = 0; j < biasOutputNumCols; j++){
                     if (logits[i][j] > 0){
@@ -132,31 +121,30 @@ int main(){
                     }
                 }
             }
-            // how much does bias1 affect biasOutput through matAdd(weightsOutput, bias1)... * dbiasOutput = how much it affects the loss
+
             for (int i = 0; i < bias1NumRows; i++){
                 for (int j = 0; j < bias1NumCols; j++){
                     dbias1[i][j] = 1 * dbiasOutput[i][j];
                 }
             }
-            // how much does weightsOutput affect biasOutput through matAdd(weightsOutput, bias1)... * dbiasOuptut = how much it affects the loss
+
             for (int i = 0; i < weightsOutputNumRows; i++){
                 for (int j = 0; j < weightsOutputNumCols; j++){
                     dweightsOutput[i][j] = 1 * dbiasOutput[i][j];
                 }
             }
-            // how much does weights1 affect weightsOutput through matmul(input, weights1)... * dweightsOutput = how much it affects the loss
+
             // matmul 768, 1 * 1, 10
             dweights1 = matmul(transpose(input, inputNumRows, inputNumCols), dweightsOutput, inputNumCols, inputNumRows, weightsOutputNumRows, weightsOutputNumCols);
 
-            // print gradients here
-
-            // optimization(add gradients * learningRate to weights and biases)
+            // optimization
             // weights1 
             for (int i = 0; i < weights1NumRows; i++){
                 for (int j = 0; j < weights1NumCols; j++){
                     weights1[i][j] += dweights1[i][j] * learningRate;
                 }
             }
+            
             // bias1
             for (int i = 0; i < bias1NumRows; i++){
                 for (int j = 0; j < bias1NumCols; j++){
@@ -164,16 +152,13 @@ int main(){
                 }
             }
 
-            // zero grad here - replace with function zeroGrad(params)
+            // zero grad
             dprobs = zero(dprobs, probsNumRows, probsNumCols);
             dlogits = zero(dlogits, logitsNumRows, logitsNumCols);
             dbiasOutput = zero(dbiasOutput, biasOutputNumRows, biasOutputNumCols);
             dbias1 = zero(dbias1, bias1NumRows, bias1NumCols);
             dweightsOutput = zero(dweightsOutput, weightsOutputNumRows, weightsOutputNumCols);
             dweights1 = zero(dweights1, weights1NumRows, weights1NumCols);
-
-            // validation here
-            //save to visualization file here
         }
     }
     return 0;
